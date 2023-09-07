bfa9c3d2d8aa74a9a43e9c9538c1a275b9cfbb4e
Dirceu Santana <dirceu.santana@gmail.com>
2023-09-07 12:32:39 -0300

    Section 3

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Dirceu Santana on 07/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
