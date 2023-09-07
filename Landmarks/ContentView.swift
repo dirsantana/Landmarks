27bafd3695c7ac6b0eea874b61e15492093836ad
Dirceu Santana <dirceu.santana@gmail.com>
2023-09-07 16:33:49 -0300

    CVV final

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-<<<<<<< HEAD
         VStack {
             MapView()
                 .ignoresSafeArea(edges: .top)
@@ -37,22 +36,10 @@ struct ContentView: View {
                 Text("Descriptive text goes here.")
             }
             .padding()
+            
             Spacer()
+            
         }
-=======
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
-            }
-        }
-        .padding()
->>>>>>> bfa9c3d2d8aa74a9a43e9c9538c1a275b9cfbb4e
     }
 }
 
